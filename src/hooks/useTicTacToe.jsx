d1ad9d0df8e97c1de81a293bcaf4e5f64edec67c
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-07 00:46:02 +0530

    feat: tic tac toe logics

--- src/hooks/useTicTacToe.jsx
+++ src/hooks/useTicTacToe.jsx
@@ -6,12 +6,37 @@ const useTicTacToe = () => {
   const [board, setBoard] = useState(initialBoard());
   const [isXNext, setIsXNext] = useState(true);
 
-  const WINNING_PATTERNS = [];
+  const WINNING_PATTERNS = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const calculateWinner = () => {};
+  const calculateWinner = (currentBoard) => {
+    for (let index = 0; index < WINNING_PATTERNS.length; index++) {
+      const [a, b, c] = WINNING_PATTERNS[index];
+
+      if (
+        currentBoard[a] &&
+        currentBoard[a] === currentBoard[b] &&
+        currentBoard[a] === currentBoard[c]
+      ) {
+        return currentBoard[a];
+      }
+    }
+
+    return null;
+  };
 
   const handleClick = (index) => {
     const winner = calculateWinner(board);
+
+    console.log(winner);
 
     if (winner || board[index]) return;
 
@@ -21,9 +46,19 @@ const useTicTacToe = () => {
     setIsXNext(!isXNext);
   };
 
-  const getStatusMessage = () => {};
+  const getStatusMessage = () => {
+    const winner = calculateWinner(board);
 
-  const resetGame = () => {};
+    if (winner) return `Player ${winner} wins!`;
+    if (!board.includes(null)) return `It's a draw`;
+
+    return `Player ${isXNext ? "X" : "O"} turn`;
+  };
+
+  const resetGame = () => {
+    setBoard(initialBoard());
+    setIsXNext(true);
+  };
 
   return { board, handleClick, getStatusMessage, resetGame };
 };
